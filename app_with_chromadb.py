cc60f829587c54adc7a59c75ddc9b6c6e7220915
Mouad Laribia <mouadlary@gmail.com>
2023-10-23 00:04:14 +0200

    update

--- app_with_chromadb.py
+++ app_with_chromadb.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 import pickle
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv, set_key
 import chromadb
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.document_loaders import PyPDFLoader, UnstructuredPDFLoader
@@ -31,22 +31,39 @@ with st.sidebar:
     add_vertical_space(5)
     st.write(':call_me_hand:*:orange[Made by **Mouad Laribia**]*:wink:')
 
-load_dotenv()
+def get_api_key():
+    t1 = st.tabs(['OpenAI API key'])
+    openai_api_key = st.text_input(
+        'OPENAI_API_KEY',
+        type='password',
+        key='OPENAI_API_KEY',
+        # on_change=,
+        label_visibility="collapsed"
+    )
 
-def store_embeddings(path, document_chunks):
-    embeddings = OpenAIEmbeddings()
+    return openai_api_key
+
+# # Set openai api key
+# openai_api_key = get_api_key()
+# if openai_api_key is not None:
+#     set_key(".env", key_to_set="OPENAI_API_KEY", value_to_set=openai_api_key, quote_mode="always")
+    
+## Get the key value
+# load_dotenv()
+
+def store_embeddings(path, document_chunks, embeddings):
     if os.path.exists(path) and 'index' in os.listdir(path):
         vector_store = Chroma(persist_directory=path, embedding_function=embeddings)
     else:
         vector_store = Chroma.from_documents(document_chunks, embeddings, path)
     vector_store.persist()
-    return vector_store, embeddings
+    return vector_store
 
-def ask_me(query, vector_store, model_name='gpt-4'):
+def ask_me(query, vector_store, openai_api_key, model_name='gpt-4'):
     retriever = vector_store.as_retriever(search_kwargs={"k":4})
     cands = retriever.get_relevant_documents(query)
     # st.write(cands)
-    llm = ChatOpenAI(model_name=model_name)
+    llm = ChatOpenAI(model_name=model_name, openai_api_key=openai_api_key)
     qa_chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
     # qa_chain = VectorDBQA.from_chain_type(llm=llm, chain_type="stuff", vectorstore=vector_store)
     # cands = qa_chain._get_docs(query)
@@ -57,7 +74,8 @@ def ask_me(query, vector_store, model_name='gpt-4'):
 
 def main():
     st.subheader(':speech_balloon: Chat with your PDF Documents :speech_balloon:')
-    
+    # st.write('Enter your OpenAI API key')
+    openai_api_key = get_api_key()
     if not os.path.exists("tmp"):
         os.mkdir("tmp")
 
@@ -74,7 +92,7 @@ def main():
         
         # chunking
         text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
+            chunk_size=2000,
             chunk_overlap=100,
             length_function=len
         )
@@ -84,14 +102,17 @@ def main():
         st.write(store_name)
 
         # embeddings
-        persistent_directory = f"db/db_{store_name}"
-        vector_store, embeddings = store_embeddings(persistent_directory, chunks)
-
+        # openai_api_key = os.getenv("OPENAI_API_KEY")
+        # print(openai_api_key)
+        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+        persistent_directory = os.path.join("db", f"db_{store_name}")
+        vector_store = store_embeddings(persistent_directory, chunks, embeddings)
+        
         # Get user questions/queries
         query = st.text_input("Now that you have uploaded your PDF document,\
                               you can ask you questions about it:")
         if query:
-            response = ask_me(query, vector_store)
+            response = ask_me(query, vector_store, openai_api_key)
             st.write(response)
 
 if __name__ == '__main__':
